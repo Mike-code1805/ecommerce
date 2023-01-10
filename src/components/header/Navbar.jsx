c7635b397ca3c8e619e90c79a0db216a656d62d5
Miguel <miguelenrique1805@gmail.com>
2023-01-10 11:18:00 -0500

    added refactor routes

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -1,11 +1,10 @@
 import styles from './styles.module.css';
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 import Badge from '@mui/material/Badge';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { routes } from '../../routes/routes';
 
 export const Navbar = () => {
-  const navigate = useNavigate();
-
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
@@ -18,17 +17,15 @@ export const Navbar = () => {
           <h1 className={styles.logo}>ECOMMERCE APP</h1>
         </div>
         <div className={styles.center}>
-          <div
-            className={styles.menuItem}
-            onClick={() => navigate('/api/products')}
-          >
-            PRODUCTOS
-          </div>
-          <Link className={styles.menuItem} to={'/api/aboutus'}>
-            ACERCA DE
-          </Link>
+          {routes.map((data, index) => (
+            <Link key={index} className={styles.menuItem} to={data.path}>
+              {data.name}
+            </Link>
+          ))}
+          {/* <div className={styles.menuItem}>PRODUCTOS</div>
+          <div className={styles.menuItem}>ACERCA DE</div>
           <div className={styles.menuItem}>REGISTRO</div>
-          <div className={styles.menuItem}>INICIAR SESION</div>
+          <div className={styles.menuItem}>INICIAR SESION</div> */}
         </div>
         <div className={styles.right}>
           <div className={styles.menuItem}>
--- src/routes/Navigation.jsx
+++ src/routes/Navigation.jsx
@@ -1,15 +1,19 @@
 import { Route, Routes } from 'react-router-dom';
-import { AboutUs, Login, MainPage, Products, Register } from '../pages';
+// import { AboutUs, Login, MainPage, Products, Register } from '../pages';
+import { routes } from './routes';
 
 export const Navigation = () => {
   return (
     <div>
       <Routes>
-        <Route path='/' element={<MainPage />} />
+        {routes.map(({ path, component: Component }, index) => (
+          <Route key={index} path={path} element={<Component />} />
+        ))}
+        {/* <Route path='/' element={<MainPage />} />
         <Route path='/api/products' element={<Products />} />
         <Route path='/api/aboutus' element={<AboutUs />} />
         <Route path='/auth/login' element={<Login />} />
-        <Route path='/auth/register' element={<Register />} />
+        <Route path='/auth/register' element={<Register />} /> */}
       </Routes>
     </div>
   );

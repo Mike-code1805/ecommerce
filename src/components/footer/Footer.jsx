6129536a0370982b29f97366f494b438ebb8de9b
Miguel <miguelenrique1805@gmail.com>
2023-01-16 10:11:02 -0500

    message

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,12 +1,3 @@
-// import {
-//   Facebook,
-//   Instagram,
-//   MailOutline,
-//   Phone,
-//   Pinterest,
-//   Room,
-//   Twitter,
-// } from '@material-ui/icons';
 import FacebookOutlinedIcon from '@mui/icons-material/FacebookOutlined';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import TwitterIcon from '@mui/icons-material/Twitter';
@@ -16,96 +7,8 @@ import { IconButton } from '@mui/material';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
 import MailOutlineIcon from '@mui/icons-material/MailOutline';
-// const Container = styled.div`
-//   display: flex;
-//   ${mobile({ flexDirection: 'column' })}
-// `;
-
-// const Left = styled.div`
-//   flex: 1;
-//   display: flex;
-//   flex-direction: column;
-//   padding: 20px;
-// `;
-
-// const Logo = styled.h1``;
-
-// const Desc = styled.p`
-//   margin: 20px 0px;
-// `;
-
-// const SocialContainer = styled.div`
-//   display: flex;
-// `;
-
-// const SocialIcon = styled.div`
-//   width: 40px;
-//   height: 40px;
-//   border-radius: 50%;
-//   color: white;
-//   background-color: #${(props) => props.color};
-//   display: flex;
-//   align-items: center;
-//   justify-content: center;
-//   margin-right: 20px;
-// `;
-
-// const Center = styled.div`
-//   flex: 1;
-//   padding: 20px;
-//   ${mobile({ display: 'none' })}
-// `;
-
-// const Title = styled.h3`
-//   margin-bottom: 30px;
-// `;
-
-// const List = styled.ul`
-//   margin: 0;
-//   padding: 0;
-//   list-style: none;
-//   display: flex;
-//   flex-wrap: wrap;
-// `;
-
-// const ListItem = styled.li`
-//   width: 50%;
-//   margin-bottom: 10px;
-//   cursor: pointer;
-// `;
-
-// const Right = styled.div`
-//   flex: 1;
-//   padding: 20px;
-//   ${mobile({ backgroundColor: '#fff8f8' })}
-// `;
-
-// const ContactItem = styled.div`
-//   margin-bottom: 20px;
-//   display: flex;
-//   align-items: center;
-// `;
-
-// const Payment = styled.img`
-//   width: 50%;
-// `;
 
 export const Footer = () => {
-  // const history = useHistory();
-
-  // const gotoTeddies = () => {
-  //   history.push('/products/teddies');
-  // };
-  // const gotoNecklaces = () => {
-  //   history.push('/products/necklaces');
-  // };
-  // const gotoKeychains = () => {
-  //   history.push('/products/keychains');
-  // };
-  // const gotoHome = () => {
-  //   history.push('/');
-  // };
-
   return (
     <div className={styles.container}>
       <div className={styles.left}>
